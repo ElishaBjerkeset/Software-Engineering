3331e0e752ce04affbb72ff1740740eab8ac6e68
ElishaBjerkeset <ejbjerkeset@gmail.com>
2025-07-05 21:25:54 -0500

    Release 1: Your program should allow adding and displaying of pets.

---
+++ CSC422BjerkesetAssignment1.java
@@ -0,0 +1,97 @@
+//Elisha Bjerkeset
+//Assignment 1 Part 2
+//07/05/2025
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CSC422BjerkesetAssignment1 {
+    public static void main(String[] args) {
+        Boolean wantToPlay = true;
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Pet> petData = new ArrayList<Pet>();
+        System.out.println("Pet Database Program.");
+
+        while(wantToPlay) {
+            System.out.println("\nWhat would you like to do?\r\n" + //
+                                "1) View all pets\r\n" + //
+                                "2) Add more pets\r\n" + //
+                                "3) Update an existing pet\r\n" + //
+                                "4) Remove an existing pet\r\n" + //
+                                "5) Search pets by name\r\n" + //
+                                "6) Search pets by age\r\n" + //
+                                "7) Exit program Your choice: ");
+            Integer userInput = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("");
+
+            switch(userInput) {
+                case 1:
+                    System.out.println("+----------------------+");
+                    System.out.println("| ID | NAME      | AGE |");
+                    System.out.println("+----------------------+");
+                    for(int i = 0; i < petData.size(); i++) {
+                        System.out.printf("| %2d | %-9s | %3d |%n", i, petData.get(i).getName(), petData.get(i).getAge());
+                    }
+                    System.out.println("+----------------------+");
+                    break;
+
+                case 2:
+                    Boolean wantToAdd = true;
+                    Integer petAddedCount = 0;
+
+                    while(wantToAdd) {
+                        System.out.print("add pet (name, age): ");
+                        String petInput = scanner.nextLine();
+                        String[] petInfo = petInput.split(" ");
+                        if("done".equals(petInfo[0])) {
+                            switch(petAddedCount) {
+                                case 0:
+                                    System.out.println("no pets added.");
+                                    break;
+                                case 1:
+                                    System.out.println("one pet added.");
+                                    break;
+                                default:
+                                    System.out.println("pets added.");
+                                    break;
+                            }
+                            wantToAdd = false;
+                        }
+                        else {
+                            petData.add(new Pet(petInfo[0], Integer.valueOf(petInfo[1])));
+                            petAddedCount += 1;
+                            System.out.println(petAddedCount);
+                        }
+                    }
+                    
+                    break;
+                    
+            }
+        }
+        scanner.close();
+    }
+
+    public static class Pet {
+        private String name;
+        private Integer age;
+
+        public Pet(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public Integer getAge() {
+            return age;
+        }
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+    }
+}
