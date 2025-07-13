916ae1f381c6e295b85b3e063ed2469b73bfcce8
ElishaBjerkeset <ejbjerkeset@gmail.com>
2025-07-13 14:34:47 -0500

    Milestone 4. Added Error Handling. Fixes #4

--- CSC422BjerkesetAssignment1.java
+++ CSC422BjerkesetAssignment1.java
@@ -2,8 +2,11 @@
 //Assignment 1 Part 2
 //07/05/2025
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -34,7 +37,7 @@ public class CSC422BjerkesetAssignment1 {
                                 "4) Remove an existing pet\r\n" + //
                                 "5) Search pets by name\r\n" + //
                                 "6) Search pets by age\r\n" + //
-                                "7) Exit program Your choice: ");
+                                "7) Exit program\nYour choice: ");
             Integer userInput = scanner.nextInt();
             scanner.nextLine();
             System.out.println("");
@@ -49,27 +52,38 @@ public class CSC422BjerkesetAssignment1 {
                     Integer petAddedCount = 0;
 
                     while(wantToAdd) {
-                        System.out.print("add pet (name, age): ");
-                        String petInput = scanner.nextLine();
-                        String[] petInfo = petInput.split(" ");
-                        if("done".equals(petInfo[0])) {
-                            switch(petAddedCount) {
-                                case 0:
-                                    System.out.println("no pets added.");
-                                    break;
-                                case 1:
-                                    System.out.println("one pet added.");
-                                    break;
-                                default:
-                                    System.out.println("pets added.");
-                                    break;
+                            System.out.print("add pet (name, age): ");
+                            String petInput = scanner.nextLine();
+                            String[] petInfo = petInput.split(" ");
+                            if("done".equals(petInfo[0])) {
+                                switch(petAddedCount) {
+                                    case 0:
+                                        System.out.println("no pets added.");
+                                        break;
+                                    case 1:
+                                        System.out.println("1 pet added.");
+                                        break;
+                                    default:
+                                        System.out.println(petAddedCount + " pets added.");
+                                        break;
+                                }
+                                wantToAdd = false;
                             }
-                            wantToAdd = false;
-                        }
-                        else {
-                            petData.add(new Pet(petInfo[0], Integer.valueOf(petInfo[1])));
-                            petAddedCount += 1;
-                        }
+                            else {
+                                if(petData.size() == 5) {
+                                    System.out.println("Error: Database is full.");
+                                }
+                                else if(petData.size() != 2) {
+                                    System.out.println("Error: " + petInput + "is not a valid input.");
+                                }
+                                else if(Integer.valueOf(petInfo[1]) > 20) {
+                                    System.out.println("Error: " + Integer.valueOf(petInfo[1]) + " is not a valid age.");
+                                }
+                                else {
+                                    petData.add(new Pet(petInfo[0], Integer.valueOf(petInfo[1])));
+                                    petAddedCount += 1;
+                                }
+                            } 
                     }
                     break;
                     case 3:
@@ -89,8 +103,14 @@ public class CSC422BjerkesetAssignment1 {
                         System.out.print("Enter the pet ID to remove: ");
                         Integer deleteNum = scanner.nextInt();
                         scanner.nextLine();
-                        System.out.print(petData.get(deleteNum).getName() + " " + petData.get(deleteNum).getAge() + " is removed. ");
-                        petData.remove((int) deleteNum);
+                        if(deleteNum >= 0 && deleteNum <= petData.size()) {
+                            System.out.println(petData.get(deleteNum).getName() + " " + petData.get(deleteNum).getAge() + " is removed. ");
+                            petData.remove((int) deleteNum);
+                        }
+                        else {
+                            System.out.println("Error: ID -1 does not exist.");
+                        }
+                        
                     break;
                     case 5:
                         System.out.print("Enter a name to search: ");
@@ -120,6 +140,16 @@ public class CSC422BjerkesetAssignment1 {
                         wantToPlay = false;
             }
         }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("pets.txt"))) {
+            for(Pet pet : petData) {
+                writer.write(pet.getName() + " " + pet.getAge());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+        }
+        System.out.println("Successfully wrote to file.");
         scanner.close();
     }
 
